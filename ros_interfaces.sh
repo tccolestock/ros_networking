118ebe3586193f5384cf65efaccc5d0d6424efaa
Thomas Colestock <tccolestock@gmail.com>
2016-06-03 17:00:44 -0400

    removed prewritten addresses

--- ros_interfaces.sh
+++ ros_interfaces.sh
@@ -6,19 +6,19 @@
 # <IP Address> is the address communicating TO
 # <Interface> is the port on this computer the communication should go through
 
+
+
 # eth0 interfaces--------------------------------------------------------------------
-sudo ip -4 ro add 192.168.0.1 dev eth0  # laptop for EEG recording
+# sudo ip -4 ro add <IP Address> dev eth0
 
 
 # eth1 interfaces--------------------------------------------------------------------
-sudo ip -4 ro add 192.168.0.3 dev eth1  # johnny's computer
-sudo ip -4 ro add 192.168.0.4 dev eth1  # shadow computer
-sudo ip -4 ro add 192.168.0.5 dev eth1  # moaed's computer
+# sudo ip -4 ro add <IP Address> dev eth1
 
 
 # eth2 interfaces--------------------------------------------------------------------
-#sudo ip -4 ro add <address> dev eth2
+#sudo ip -4 ro add <IP Address> dev eth2
 
 
 # wlan0 interfaces--------------------------------------------------------------------
-#sudo ip -4 ro add <address> dev wlan0
+#sudo ip -4 ro add <IP Address> dev wlan0
